4913888f23edf05674de33c8f81a035e5484235e
Valerii Sergheiev <Val.Sergeyev@gmail.com>
2015-09-12 16:54:50 +0300

    select size

--- js/common.js
+++ js/common.js
@@ -1,18 +1,9 @@
 $(document).ready(function(){  
-
-    $('.pagination ul li').click(function(){  
-        $.ajax({  
-            url: "http://eportal.com.ua/admin.php",
-            cache: false,  
-            dataType: "json";
-            success: function(html){  
-            $(".pagination").html(html);  
-            }
-        });  
+  
+    $('.circles').click(function(){
+        $('.circles-click').removeClass('circles-click');
+        $(this).addClass('circles-click');
+        
     });
-
-$('.pagination ul li').click(function(){
-    $('.pagination p').text('rfrfrfrfr');
-});
 
 });  
