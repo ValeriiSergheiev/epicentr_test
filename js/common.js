c638dfb8df8f07f3233eb7dbc786ea5894da1e1e
Valerii Sergheiev <Val.Sergeyev@gmail.com>
2015-09-13 13:03:03 +0300

    js changed

--- js/common.js
+++ js/common.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){  
   
+    $('.menu ul li').click(function(){
+        $('.menu ul li').removeClass('menu-active');
+        $(this).addClass('menu-active');
+    });
+
     $('.circles').click(function(){
         $('.circles-click').removeClass('circles-click');
         $(this).addClass('circles-click');
         
-    });
+        $.get( "/epik.json", function( data ) {
+            var data=JSON.parse(data);
+            console.log(data[8].DESCRIPTION);
+        });
 
+    });
 });  
